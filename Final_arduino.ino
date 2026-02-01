9568fa6db1610b6408569797995776ae1f0d0c4f
Arita Z. <zaman.arita@gmail.com>
2026-01-31 21:38:24 -0500

    Add MPU6050 setup and slouch detection
    Initialize MPU6050 and implement slouch detection logic.

---
+++ Final_arduino.ino
@@ -0,0 +1,76 @@
+#include <Wire.h>
+
+#include <MPU6050.h>
+
+ 
+
+MPU6050 imu;
+
+ 
+
+// change this number later to tune sensitivity
+
+int SLOUCH_THRESHOLD = 15000;
+
+ 
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+
+  imu.initialize();
+
+  // Wake up the MPU6050
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  Serial.println(imu.testConnection() ? "MPU OK" : "MPU FAIL");
+}
+ 
+
+void loop() {
+
+  int16_t ax, ay, az;
+
+  imu.getAcceleration(&ax, &ay, &az);
+
+  // Serial.print("AY: ");
+  // Serial.println(ay);
+
+  
+ 
+
+  // ay = forward/back tilt
+
+  if (ay < 14500) {
+
+    digitalWrite(4, LOW);
+
+    digitalWrite(3, HIGH);
+
+    Serial.println("SLOUCH");
+  } else if (ay < 15500) {
+    
+    analogWrite(4, 75);
+
+    digitalWrite(3, HIGH);
+    Serial.println("GOOD");
+
+
+  } else {
+
+    digitalWrite(4, HIGH);
+
+    digitalWrite(3, LOW);
+    Serial.println("GOOD");
+
+
+  }
+
+ 
+
+  delay(200);
+
+}
